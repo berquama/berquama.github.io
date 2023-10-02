ca1463c7c2db951fad07308679f5a73c76df4fd8
Austin Berquam <berquama@gmail.com>
2023-10-02 14:17:34 -0500

    css changes

--- js/scripts.js
+++ js/scripts.js
@@ -3,10 +3,14 @@ function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
   document.getElementById("main").style.marginLeft = "250px";
   document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  document.getElementById("openButton").style.display = "none";
+  document.getElementById("closeButton").style.display = "block";
 }
 
 function closeNav() {
-  document.getElementById("mySidenav").style.width = "120px";
-  document.getElementById("main").style.marginLeft= "120px";
+  document.getElementById("mySidenav").style.width = "100px";
+  document.getElementById("main").style.marginLeft= "100px";
   document.body.style.backgroundColor = "white";
+  document.getElementById("openButton").style.display = "block";
+  document.getElementById("closeButton").style.display = "none";
 }
