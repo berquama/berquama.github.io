9d3779a25331e0443114e0056e2a6f0ba1359ebb
Austin Berquam <berquama@gmail.com>
2023-10-04 10:03:10 -0500

    almost finished the nav

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,50 @@
-
 function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
-  document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+	hideNavImages();
+	expandNav();
+	changeButtonsOpen();
+}
+function closeNav() {
+	showNavImages();
+	shrinkNav();
+	changeButtonsClose();
+}
+
+function changeButtonsClose() {
+  document.getElementById("openButton").style.display = "block";
+  document.getElementById("closeButton").style.display = "none";
+}
+
+function changeButtonsOpen() {
   document.getElementById("openButton").style.display = "none";
   document.getElementById("closeButton").style.display = "block";
 }
 
-function closeNav() {
+function expandNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+  var cols = document.getElementsByClassName('overlay');
+  for(i = 0; i < cols.length; i++) {
+    cols[i].style.opacity = '0.8';
+  }
+}
+
+function shrinkNav() {
   document.getElementById("mySidenav").style.width = "100px";
   document.getElementById("main").style.marginLeft= "100px";
-  document.body.style.backgroundColor = "white";
-  document.getElementById("openButton").style.display = "block";
-  document.getElementById("closeButton").style.display = "none";
 }
+
+function hideNavImages() {
+  var cols = document.getElementsByClassName('nav-image');
+  for(i = 0; i < cols.length; i++) {
+    cols[i].style.display = 'none';
+  }
+}
+  
+function showNavImages() {
+  var cols = document.getElementsByClassName('nav-image');
+  for(i = 0; i < cols.length; i++) {
+    cols[i].style.display = 'block';
+  }
+}
+
+	
