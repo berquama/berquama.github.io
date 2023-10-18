8f3bc28202641500b530ad8903055fdfce4474c5
Austin Berquam <berquama@gmail.com>
2023-10-18 18:59:25 -0500

    big changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const links = document.querySelectorAll('nav a');
 
+    links.forEach(link => {
+        link.addEventListener('click', function (event) {
+            event.preventDefault();
+            const targetId = link.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            window.scrollTo({
+                top: targetElement.offsetTop - document.querySelector('nav').clientHeight,
+                behavior: 'smooth'
+            });
+        });
+    });
+});
 
 	
